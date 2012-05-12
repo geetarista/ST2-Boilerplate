fe29646dfa79bde51b4055fbbb0049445526b02a
Robby Colvin <geetarista@gmail.com>
2012-05-12 15:12:09 -0700

    Beef it up

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 Sublime Text 2 plugin.
 
+## About
+
+This is a boilerplate for creating plugins with Sbulime Text 2.
+
+## Authoring Help
+
+- [Official documentation](http://docs.sublimetext.info/en/latest/index.html)
+- [API reference](http://www.sublimetext.com/docs/2/api_reference.html)
+- [How to Create a Sublime Text 2 Plugin](http://net.tutsplus.com/tutorials/python-tutorials/how-to-create-a-sublime-text-2-plugin/)
+- Set up [Sublime Package Control](http://wbond.net/sublime_packages/package_control/package_developers)
+
 ## License
 
 MIT. See `LICENSE`.
--- plugin.py
+++ plugin.py
@@ -1,4 +1,26 @@
-import os
-import subprocess
+# These two are required for plugin development
 import sublime
 import sublime_plugin
+
+# Usually necessary for commands
+import os
+import subprocess
+
+# Helper method
+def helper_method(something):
+    return something
+
+class ApplicationCommand(sublime_plugin.ApplicationCommand):
+    """ This is an application command """
+    def run(self):
+        something = helper_method("application")
+
+class WindowCommand(sublime_plugin.WindowCommand):
+    """ This is a window command """
+    def run(self):
+        something = helper_method("window")
+
+class LocalHelpCommand(sublime_plugin.TextCommand):
+    """ This is a text command """
+    def run(self, edit):
+        something = helper_method("text")
